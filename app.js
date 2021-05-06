5975195163f28a3ebfee8ef5a2d1861655556213
lilyhi <79773301+lilyhi@users.noreply.github.com>
2021-05-05 22:35:14 -0700

    test

--- app.js
+++ app.js
@@ -20,12 +20,17 @@ const promptUser = () => {
 };
 
 
-const promptProject = () => {
+const promptProject = portfolioData => {
+    // If there's no 'projects' array property, create one
     console.log(`
 =================
 Add a New Project
 =================
 `);
+
+if (!portfolioData.projects) {
+    portfolioData.projects = [];
+}
     return inquirer.prompt([
         {
             type: 'input',
@@ -60,13 +65,23 @@ Add a New Project
             message: 'Would you like to enter another project?',
             default: false
         }
+        .then(projectData => {
+            portfolioData.projects.push(projectData);
+            if (projectData.confirmAddProject) {
+                return promptProject(portfolioData);
+            }   else {
+                return portfolioData;
+            }
+        })
     ]);
 };
 
 promptUser()
-    .then(answers => console.log(answers))
     .then(promptProject)
-    .then(projectAnswers => console.log(projectAnswers));
+    .then(portfolioData => {
+        console.log(projectAnswers);
+    });
+    
 
 // const fs = require('fs');
 // const generatePage = require('./src/page-template.js');
