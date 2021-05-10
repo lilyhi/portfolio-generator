ae1986ea1184734e956d17807c4bde57c4b0543b
lilyhi <79773301+lilyhi@users.noreply.github.com>
2021-05-09 23:35:04 -0700

    finished portfolio site

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-const fs = require('fs');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
-// line 3 was added in the code snapshot
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -59,6 +58,7 @@ const promptProject = portfolioData => {
 Add a New Project
 =================
 `);
+
     // If there's no 'projects' array property, create one
     if (!portfolioData.projects) {
         portfolioData.projects = [];
@@ -136,14 +136,44 @@ Add a New Project
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        const pageHTML = generatePage(portfolioData);
+        return generatePage(portfolioData);
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
+    });
 
-        // fs.writeFile('./index.html', pageHTML, err => {
-        //   if (err) throw new Error(err);
 
-        //   console.log('Page created! Check out index.html in this directory to see it!');
-        // });
-});
+
+// promptUser()
+//     .then(promptProject)
+//     .then(portfolioData => {
+//         const pageHTML = generatePage(portfolioData);
+
+//         fs.writeFile('./dist/index.html', pageHTML, err => {
+//             if (err) {
+//                 console.log(err);
+//                 return;
+//             }
+//             console.log('Page created! Check out index.html in this directory to see it!');
+
+//             fs.copyFile('./src/style.css', './dist/style.css', err => {
+//                 if (err) {
+//                     console.log(err);
+//                     return;
+//                 }
+//                 console.log('Style sheet copied successfully');
+//         });
+// });
     
 
 
@@ -151,7 +181,7 @@ promptUser()
 // const pageHTML = generatePage(name, github);
 
 // fs.writeFile('./index.html', pageHTML, err => {
-//     if (err) throw err;
+    // if (err) throw new Error(err);
 
 //     console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+// })
